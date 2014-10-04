cf7e7e9c98c40aa38a1bf5eedee432e8c0150463
catlag <sammieeagle@gmail.com>
2014-10-03 17:50:24 -0700

    added x and o and function to not change between`

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
+
+var letter= "X";
+
 var styleBoxElements = function(){
-var gameBoxes = document.querySelectorAll(".box");
+var gameBoxes = document.querySelectorAll("td");
 for (var i = 0; i < gameBoxes.length; i++) {
 gameBoxes[i].addEventListener("click", selectItem);
-console.log(gameBoxes[i]);
 }
 };
 
 
 
-var selectItem = function( event ){
-	console.log("Clicked item:"+this + event);
-document.querySelector("img").setAttribute("src","./cross.png");
+var selectItem = function(){
+	// console.log("Clicked item:"+this + event);
+	if (this.innerText === ""){
+	this.innerText = letter;
+	nextItem();
+	}
 };
 
-styleBoxElements();
+function nextItem() {
+	if (letter === "X") {
+		letter = "O";
+}else {
+	letter = "X";
+}
+};
+
+
+var initaliaze = function(){
+	styleBoxElements();
+};
+
+window.onload = initaliaze;
+
+// document.getElementById("box1").addEventListener("click", selectItem);
+// document.getElementById("box2").addEventListener("click", selectItem);
